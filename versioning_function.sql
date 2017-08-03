606fe6ea77d28601133095cae8393b224eec0432
Paolo Chiodi <chiodi84@gmail.com>
2017-08-03 11:55:32 +0200

    Performance optimisations And user adjusted period for update in original table as well

--- versioning_function.sql
+++ versioning_function.sql
@@ -5,8 +5,9 @@ DECLARE
   sys_period text;
   history_table text;
   manipulate jsonb;
-  new_period tstzrange;
   commonColumns text[];
+  time_stamp_to_use timestamptz := current_timestamp;
+  range_lower timestamptz;
 BEGIN
   IF TG_WHEN != 'BEFORE' OR TG_LEVEL != 'ROW' THEN
     RAISE TRIGGER_PROTOCOL_VIOLATED USING
@@ -27,7 +28,7 @@ BEGIN
   sys_period := TG_ARGV[0];
   history_table := TG_ARGV[1];
 
-
+  -- check if sys_period exists on original table
   IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
     IF NOT EXISTS(SELECT * FROM pg_attribute WHERE attrelid = TG_TABLE_NAME::regclass AND attname = sys_period) THEN
       RAISE 'column "%" of relation "%" does not exist', sys_period, TG_TABLE_NAME;
@@ -35,10 +36,12 @@ BEGIN
   END IF;
 
   IF TG_OP = 'UPDATE' OR TG_OP = 'DELETE' THEN
+    -- check if history table exits
     IF NOT EXISTS(SELECT * FROM pg_tables WHERE tablename = history_table AND schemaname = TG_TABLE_SCHEMA) THEN
       RAISE 'relation "%" does not exist', history_table;
     END IF;
 
+    -- check if history table has sys_period
     IF NOT EXISTS(SELECT * FROM pg_attribute WHERE attrelid = history_table::regclass AND attname = sys_period) THEN
       RAISE 'history relation "%" does not contain system period column "%"', history_table, sys_period USING
       HINT = 'history relation must contain system period column with the same name and data type as the versioned one';
@@ -46,35 +49,30 @@ BEGIN
 
     IF TG_ARGV[2] = 'true' THEN
       -- mitigate update conflicts
-      EXECUTE format('
-        SELECT CASE WHEN lower($1.%I) < current_timestamp
-        THEN tstzrange(lower($1.%I), current_timestamp, ''[)'')
-        ELSE tstzrange(lower($1.%I), lower($1.%I) + interval ''1 microseconds'', ''[)'')
-        END', sys_period, sys_period, sys_period, sys_period) USING OLD INTO new_period;
-    ELSE
-      EXECUTE format('SELECT tstzrange(lower($1.%I), current_timestamp, ''[)'')', sys_period) USING OLD INTO new_period;
-    END IF;
-    -- manipulate := jsonb_set('{}'::jsonb, ('{' || sys_period || '}')::text[], to_jsonb(new_period));
+      EXECUTE format('SELECT lower($1.%I)', sys_period) USING OLD INTO range_lower;
 
-    EXECUTE 'WITH history AS
+      IF range_lower >= time_stamp_to_use THEN
+        time_stamp_to_use := range_lower + interval '1 microseconds';
+      END IF;
+    END IF;
+
+    WITH history AS
       (SELECT attname
       FROM   pg_attribute
-      WHERE  attrelid = $1::regclass
+      WHERE  attrelid = history_table::regclass
       AND    attnum > 0
       AND    NOT attisdropped),
       main AS
       (SELECT attname
       FROM   pg_attribute
-      WHERE  attrelid = $2::regclass
+      WHERE  attrelid = TG_TABLE_NAME::regclass
       AND    attnum > 0
       AND    NOT attisdropped)
-      SELECT array_agg(quote_ident(history.attname))
+    SELECT array_agg(quote_ident(history.attname)) INTO commonColumns
       FROM history
       INNER JOIN main
       ON history.attname = main.attname
-      AND history.attname != $3'
-    USING history_table, TG_TABLE_NAME, sys_period
-    INTO commonColumns;
+      AND history.attname != sys_period;
 
     EXECUTE ('INSERT INTO ' ||
       quote_ident(history_table) ||
@@ -84,14 +82,12 @@ BEGIN
       quote_ident(sys_period) ||
       ') VALUES ($1.' ||
       array_to_string(commonColumns, ',$1.') ||
-      ',$2)')
-       USING OLD, new_period;
+      ',tstzrange($2, $3, ''[)''))')
+       USING OLD, range_lower, time_stamp_to_use;
   END IF;
 
   IF TG_OP = 'UPDATE' OR TG_OP = 'INSERT' THEN
-
-
-    manipulate := jsonb_set('{}'::jsonb, ('{' || sys_period || '}')::text[], to_jsonb(tstzrange(current_timestamp, null, '[)')));
+    manipulate := jsonb_set('{}'::jsonb, ('{' || sys_period || '}')::text[], to_jsonb(tstzrange(time_stamp_to_use, null, '[)')));
 
     RETURN jsonb_populate_record(NEW, manipulate);
   END IF;
