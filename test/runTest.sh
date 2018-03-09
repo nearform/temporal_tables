529792629329c9d24130c6709d11bcb3aa9eabd5
Ivan Jovanovic <ivanjov96@gmail.com>
2018-03-09 03:22:16 +0100

    Circle CI integration

--- test/runTest.sh
+++ test/runTest.sh
@@ -10,13 +10,26 @@ TESTS="
   invalid_system_period_values invalid_system_period invalid_types
   versioning structure combinations different_schema"
 
+error=0
+
 for name in $TESTS; do
   echo ""
   echo $name
   echo ""
   psql temporal_tables_test -X -a -q --set=SHOW_CONTEXT=never < test/sql/$name.sql > test/result/$name.out 2>&1
-  diff -b test/expected/$name.out test/result/$name.out
+  difference="$(diff -b test/expected/$name.out test/result/$name.out)"
+  echo $difference
+  if [ ! -z "$difference" ]; then
+    error=1
+  fi
 done
 
 
 psql -q -c "drop database temporal_tables_test;"
+
+echo $error
+if [ "$error" -eq 1 ]; then
+  exit 1
+fi
+
+exit 0
