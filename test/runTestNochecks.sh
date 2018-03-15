beace43f3f1f24dc3763c36bc1bd02786672ce3a
Ivan Jovanovic <ivanjov96@gmail.com>
2018-03-15 18:42:32 +0100

    Connect to db via tcp

--- test/runTestNochecks.sh
+++ test/runTestNochecks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-createdb temporal_tables_test
-psql temporal_tables_test -q -f versioning_function_nochecks.sql
+createdb -h 127.0.0.1 -p 5432 -U postgres temporal_tables_test
+psql -h 127.0.0.1 -p 5432 -U postgres temporal_tables_test -q -f versioning_function_nochecks.sql
 
 mkdir -p test/result
 
@@ -13,7 +13,7 @@ for name in $TESTS; do
   echo ""
   echo $name
   echo ""
-  psql temporal_tables_test -X -a -q --set=SHOW_CONTEXT=never < test/sql/$name.sql > test/result/$name.out 2>&1
+  psql -h 127.0.0.1 -p 5432 -U postgres temporal_tables_test -X -a -q --set=SHOW_CONTEXT=never < test/sql/$name.sql > test/result/$name.out 2>&1
   difference="$(diff -b test/expected/$name.out test/result/$name.out)"
   echo $difference
   if [ ! -z "$difference" ]; then
