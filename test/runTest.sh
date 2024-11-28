923ffb5b81dda785535c5200ac85a6647571181e
Mohammed Bilal Shareef <bilalshareef.h@gmail.com>
2024-11-28 19:51:12 +0530

    feat: Remove tests duplication by leveraging original extension's test against this one (#79)
    * feat: Remove tests duplication by leveraging original extension's test against this one
    
    * Set date style to Postgres
    
    * Update expected outputs
    
    * Refactor shell scripts and remove unused test files
    
    * Test CI failure case
    
    * Revert: Test CI failure case
    
    * Address review comments
    
    * Fix CI failure

--- test/runTest.sh
+++ test/runTest.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+export PGDATESTYLE="Postgres, MDY";
+
 createdb temporal_tables_test
 psql temporal_tables_test -q -f versioning_function.sql
 psql temporal_tables_test -q -f system_time_function.sql
 
 mkdir -p test/result
 
+FILES_DIFFERENT=false
+
+REMOTE_TESTS="
+  combinations structure versioning
+  invalid_system_period invalid_system_period_values invalid_types
+  no_history_system_period no_history_table no_system_period
+  "
+
+./test/runRemoteTests.sh "$REMOTE_TESTS"
+REMOTE_TESTS_RESULT=$?
+
+if [ "$REMOTE_TESTS_RESULT" -eq 1 ]; then
+  # Atleast one of the remote tests failed.
+  FILES_DIFFERENT=true
+fi
+
 TESTS="
-  no_history_table no_history_system_period no_system_period
-  invalid_system_period_values invalid_system_period invalid_types
-  versioning upper_case structure combinations
-  different_schema unchanged_values unchanged_version_values
+  upper_case different_schema unchanged_values unchanged_version_values
   non_equality_types non_equality_types_unchanged_values
   set_system_time invalid_set_system_time versioning_including_current_version_in_history
   versioning_rollback_include_current_version_in_history noop_update
@@ -21,8 +36,18 @@ for name in $TESTS; do
   echo $name
   echo ""
   psql temporal_tables_test -X -a -q --set=SHOW_CONTEXT=never < test/sql/$name.sql > test/result/$name.out 2>&1
-  diff -b test/expected/$name.out test/result/$name.out
+  DIFF_OUTPUT=$(diff -b test/expected/$name.out test/result/$name.out)
+  echo "$DIFF_OUTPUT"
+
+  if [ -n "$DIFF_OUTPUT" ]; then
+    # Expected and actual files are different.
+    FILES_DIFFERENT=true
+  fi
 done
 
-
 psql -q -c "drop database temporal_tables_test;"
+
+# Exit with 1 if any of the test case failed.
+if [ "$FILES_DIFFERENT" = true ]; then
+  exit 1
+fi
--- test/sql/no_system_period.sql
+++
@@ -1,9 +0,0 @@
-SET client_min_messages TO error;
-
-CREATE TABLE no_system_period ();
-
-CREATE TRIGGER versioning_trigger
-BEFORE INSERT OR UPDATE OR DELETE ON no_system_period
-FOR EACH ROW EXECUTE PROCEDURE versioning(NULL, NULL, false);
-
-INSERT INTO no_system_period DEFAULT VALUES;
