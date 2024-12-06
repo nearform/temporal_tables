38068a60c38c18c156798c52b4d8f4e951eab474
Mohammed Bilal Shareef <bilalshareef.h@gmail.com>
2024-12-06 14:19:34 +0530

    Add tests for null column fix of current version in history feature

--- test/sql/versioning_including_current_version_in_history.sql
+++ test/sql/versioning_including_current_version_in_history.sql
@@ -1,10 +1,10 @@
-CREATE TABLE versioning (a bigint, "b b" date, sys_period tstzrange);
+CREATE TABLE versioning (a bigint, x bigint, "b b" date, sys_period tstzrange);
 
 -- Insert some data before versioning is enabled.
-INSERT INTO versioning (a, sys_period) VALUES (1, tstzrange('-infinity', NULL));
-INSERT INTO versioning (a, sys_period) VALUES (2, tstzrange('2000-01-01', NULL));
+INSERT INTO versioning (a, x, sys_period) VALUES (1, 1, tstzrange('-infinity', NULL));
+INSERT INTO versioning (a, x, sys_period) VALUES (2, 2, tstzrange('2000-01-01', NULL));
 
-CREATE TABLE versioning_history (a bigint, c date, sys_period tstzrange);
+CREATE TABLE versioning_history (a bigint, x bigint, c date, sys_period tstzrange);
 
 CREATE TRIGGER versioning_trigger
 BEFORE INSERT OR UPDATE OR DELETE ON versioning
@@ -27,7 +27,7 @@ SELECT pg_sleep(0.1);
 -- Update.
 BEGIN;
 
-UPDATE versioning SET a = 4 WHERE a = 3;
+UPDATE versioning SET a = 4, x = 4 WHERE a = 3;
 
 SELECT a, "b b", lower(sys_period) = CURRENT_TIMESTAMP FROM versioning ORDER BY a, sys_period;
 
