77237a50d9b4d42f058d85ed315aa553b1360554
Michael Scott <michael.scott123@t-mobile.com>
2025-06-23 20:52:19 -0700

    feat: WIP

--- generate_static_versioning_trigger.sql
+++ generate_static_versioning_trigger.sql
@@ -82,6 +82,14 @@ BEGIN
     FROM pg_attribute
    WHERE attrelid = p_history_table::regclass AND attname = p_sys_period AND NOT attisdropped;
 
+  -- Check sys_period type at render time
+  IF sys_period_type != 'tstzrange' THEN
+    RAISE 'system period column %% does not have type tstzrange', %2$L;
+  END IF;
+  IF history_sys_period_type != 'tstzrange' THEN
+    RAISE 'history system period column %% does not have type tstzrange', %2$L;
+  END IF;
+
   func_sql := format($outer$
 CREATE OR REPLACE FUNCTION %1$I()
 RETURNS TRIGGER AS $func$
@@ -108,46 +116,40 @@ BEGIN
     RAISE TRIGGER_PROTOCOL_VIOLATED USING MESSAGE = 'function must be fired for INSERT or UPDATE or DELETE';
   END IF;
 
-  -- Check sys_period type at render time
-  IF %2$L != 'tstzrange' THEN
-    RAISE 'system period column %% does not have type tstzrange', %3$L;
-  END IF;
-  IF %4$L != 'tstzrange' THEN
-    RAISE 'history system period column %% does not have type tstzrange', %3$L;
+  IF %3$L AND TG_OP = 'UPDATE' THEN 
+    IF (%4$s) IS NOT DISTINCT FROM (%5$s) THEN
+      RETURN OLD;
+    END IF;
   END IF;
 
-  IF %5$L AND TG_OP = 'UPDATE' AND (%6$s) IS NOT DISTINCT FROM (%7$s) THEN
-    RETURN OLD;
-  END IF;
-
-  IF TG_OP = 'UPDATE' OR TG_OP = 'DELETE' OR (%8$L AND TG_OP = 'INSERT') THEN
-    existing_range := OLD.%3$I;
+  IF TG_OP = 'UPDATE' OR TG_OP = 'DELETE' OR (%6$L AND TG_OP = 'INSERT') THEN
+    existing_range := OLD.%2$I;
     IF existing_range IS NULL THEN
-      RAISE 'system period column %% must not be null', %3$L;
+      RAISE 'system period column %% must not be null', %2$L;
     END IF;
     IF isempty(existing_range) OR NOT upper_inf(existing_range) THEN
-      RAISE 'system period column %% contains invalid value', %3$L;
+      RAISE 'system period column %% contains invalid value', %2$L;
     END IF;
     range_lower := lower(existing_range);
     IF range_lower >= time_stamp_to_use THEN
       time_stamp_to_use := range_lower + interval '1 microseconds';
     END IF;
 
-    IF %8$L THEN
+    IF %6$L THEN
       IF TG_OP = 'UPDATE' OR TG_OP = 'DELETE' THEN
-        UPDATE %9$I SET %3$I = tstzrange(range_lower, time_stamp_to_use, '[)')
-        WHERE (%10$s) = (%10$s) AND %3$I = OLD.%3$I;
+        UPDATE %7$I SET %2$I = tstzrange(range_lower, time_stamp_to_use, '[)')
+        WHERE (%8$s) = (%8$s) AND %2$I = OLD.%2$I;
       END IF;
       IF TG_OP = 'UPDATE' OR TG_OP = 'INSERT' THEN
-        INSERT INTO %9$I (%10$s, %3$I) VALUES (%6$s, tstzrange(time_stamp_to_use, NULL, '[)'));
+        INSERT INTO %7$I (%8$s, %2$I) VALUES (%4$s, tstzrange(time_stamp_to_use, NULL, '[)'));
       END IF;
     ELSE
-      INSERT INTO %9$I (%10$s, %3$I) VALUES (%7$s, tstzrange(range_lower, time_stamp_to_use, '[)'));
+      INSERT INTO %7$I (%8$s, %2$I) VALUES (%5$s, tstzrange(range_lower, time_stamp_to_use, '[)'));
     END IF;
   END IF;
 
   IF TG_OP = 'UPDATE' OR TG_OP = 'INSERT' THEN
-    NEW.%3$I := tstzrange(time_stamp_to_use, NULL, '[)');
+    NEW.%2$I := tstzrange(time_stamp_to_use, NULL, '[)');
     RETURN NEW;
   END IF;
 
@@ -155,27 +157,25 @@ BEGIN
 END;
 $func$ LANGUAGE plpgsql;
 $outer$,
-  trigger_func_name,           -- 1
-  sys_period_type,             -- 2
-  p_sys_period,                -- 3
-  history_sys_period_type,     -- 4
-  p_ignore_unchanged_values,   -- 5
-  new_row_compare,             -- 6
-  old_row_compare,             -- 7
-  p_include_current_version_in_history, -- 8
-  p_history_table,             -- 9
-  common_columns               -- 10
+  trigger_func_name,                    
+  p_sys_period,                         
+  p_ignore_unchanged_values,            
+  new_row_compare,                      
+  old_row_compare,                      
+  p_include_current_version_in_history, 
+  p_history_table,                      
+  common_columns                        
 );
 
   trigger_sql := format($t$
 DROP TRIGGER IF EXISTS %1$I ON %2$I;
 CREATE TRIGGER %1$I
 BEFORE INSERT OR UPDATE OR DELETE ON %2$I
-FOR EACH ROW EXECUTE FUNCTION %3$I();
+FOR EACH ROW EXECUTE FUNCTION %2$I();
 $t$,
-  trigger_name,    -- 1
-  p_table_name,    -- 2
-  trigger_func_name -- 3
+  trigger_name,     
+  p_table_name,     
+  trigger_func_name 
 );
 
   RETURN func_sql || E'\n' || trigger_sql;
