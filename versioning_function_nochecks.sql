4d47b7b836ef9e3c05befd554b12b13764655fca
Austin Beer <asbeer@gmail.com>
2025-09-03 15:14:38 -0400

    Copy versioning changes to nochecks script

--- versioning_function_nochecks.sql
+++ versioning_function_nochecks.sql
@@ -10,10 +10,13 @@ DECLARE
   ignore_unchanged_values bool;
   include_current_version_in_history bool;
   enable_migration_mode bool;
+  increment_version bool;
+  version_column_name text;
   commonColumns text[];
   time_stamp_to_use timestamptz;
   range_lower timestamptz;
   existing_range tstzrange;
+  existing_version integer;
   newVersion record;
   oldVersion record;
   user_defined_system_time text;
@@ -40,6 +43,8 @@ BEGIN
   ignore_unchanged_values := COALESCE(TG_ARGV[3],'false');
   include_current_version_in_history := COALESCE(TG_ARGV[4],'false');
   enable_migration_mode := COALESCE(TG_ARGV[5],'false');
+  increment_version := COALESCE(TG_ARGV[6],'false');
+  version_column_name := COALESCE(TG_ARGV[7],'version');
 
   IF ignore_unchanged_values AND TG_OP = 'UPDATE' THEN
     IF NEW IS NOT DISTINCT FROM OLD THEN
@@ -54,7 +59,6 @@ BEGIN
         IF TG_OP = 'DELETE' THEN
           RETURN OLD;
         END IF;
-
         RETURN NEW;
       END IF;
     END IF;
@@ -66,7 +70,7 @@ BEGIN
       range_lower := lower(existing_range);
 
       IF mitigate_update_conflicts = 'true' THEN
-      -- mitigate update conflicts
+        -- mitigate update conflicts
         IF range_lower >= time_stamp_to_use THEN
           time_stamp_to_use := range_lower + interval '1 microseconds';
         END IF;
@@ -91,6 +95,10 @@ BEGIN
       ON history.attname = main.attname
       AND history.attname != sys_period;
 
+    IF increment_version = 'true' THEN
+      commonColumns := array_remove(commonColumns, quote_ident(version_column_name));
+    END IF;
+
     -- Check if record exists in history table for migration mode
     IF enable_migration_mode = 'true' AND include_current_version_in_history = 'true' AND (TG_OP = 'UPDATE' OR TG_OP = 'DELETE') THEN
       EXECUTE 'SELECT EXISTS (
@@ -103,16 +111,31 @@ BEGIN
 
       IF NOT record_exists THEN
         -- Insert current record into history table with its original range
-        EXECUTE 'INSERT INTO ' ||
-          history_table ||
-          '(' ||
-          array_to_string(commonColumns, ',') ||
-          ',' ||
-          quote_ident(sys_period) ||
-          ') VALUES ($1.' ||
-          array_to_string(commonColumns, ',$1.') ||
-          ',tstzrange($2, $3, ''[)''))'
-        USING OLD, range_lower, time_stamp_to_use;
+        IF increment_version = 'true' THEN
+          EXECUTE 'INSERT INTO ' ||
+            history_table ||
+            '(' ||
+            array_to_string(commonColumns, ',') ||
+            ',' ||
+            quote_ident(sys_period) ||
+            ',' ||
+            quote_ident(version_column_name) ||
+            ') VALUES ($1.' ||
+            array_to_string(commonColumns, ',$1.') ||
+            ',tstzrange($2, $3, ''[)''), $4)'
+          USING OLD, range_lower, time_stamp_to_use, existing_version;
+        ELSE
+          EXECUTE 'INSERT INTO ' ||
+            history_table ||
+            '(' ||
+            array_to_string(commonColumns, ',') ||
+            ',' ||
+            quote_ident(sys_period) ||
+            ') VALUES ($1.' ||
+            array_to_string(commonColumns, ',$1.') ||
+            ',tstzrange($2, $3, ''[)''))'
+          USING OLD, range_lower, time_stamp_to_use;
+        END IF;
       END IF;
     END IF;
 
@@ -151,33 +174,68 @@ BEGIN
       END IF;
       -- If we are including the current version in the history and the operation is an insert or update, we need to insert the current version in the history table
       IF TG_OP = 'UPDATE' OR TG_OP = 'INSERT' THEN
-        EXECUTE ('INSERT INTO ' ||
-          history_table ||
-          '(' ||
-          array_to_string(commonColumns , ',') ||
-          ',' ||
-          quote_ident(sys_period) ||
-          ') VALUES ($1.' ||
-          array_to_string(commonColumns, ',$1.') ||
-          ',tstzrange($2, NULL, ''[)''))')
-          USING NEW, time_stamp_to_use;
-      END IF;    
+        IF increment_version = 'true' THEN
+          EXECUTE ('INSERT INTO ' ||
+            history_table ||
+            '(' ||
+            array_to_string(commonColumns , ',') ||
+            ',' ||
+            quote_ident(sys_period) ||
+            ',' ||
+            quote_ident(version_column_name) ||
+            ') VALUES ($1.' ||
+            array_to_string(commonColumns, ',$1.') ||
+            ',tstzrange($2, NULL, ''[)''), $3)')
+            USING NEW, time_stamp_to_use, existing_version + 1;
+        ELSE
+          EXECUTE ('INSERT INTO ' ||
+            history_table ||
+            '(' ||
+            array_to_string(commonColumns , ',') ||
+            ',' ||
+            quote_ident(sys_period) ||
+            ') VALUES ($1.' ||
+            array_to_string(commonColumns, ',$1.') ||
+            ',tstzrange($2, NULL, ''[)''))')
+            USING NEW, time_stamp_to_use;
+        END IF;
+      END IF;
     ELSE
-      EXECUTE ('INSERT INTO ' ||
-      history_table ||
-      '(' ||
-      array_to_string(commonColumns , ',') ||
-      ',' ||
-      quote_ident(sys_period) ||
-      ') VALUES ($1.' ||
-      array_to_string(commonColumns, ',$1.') ||
-      ',tstzrange($2, $3, ''[)''))')
-       USING OLD, range_lower, time_stamp_to_use;
+      IF increment_version = 'true' THEN
+        EXECUTE ('INSERT INTO ' ||
+        history_table ||
+        '(' ||
+        array_to_string(commonColumns , ',') ||
+        ',' ||
+        quote_ident(sys_period) ||
+        ',' ||
+        quote_ident(version_column_name) ||
+        ') VALUES ($1.' ||
+        array_to_string(commonColumns, ',$1.') ||
+        ',tstzrange($2, $3, ''[)''), $4)')
+         USING OLD, range_lower, time_stamp_to_use, existing_version;
+      ELSE
+        EXECUTE ('INSERT INTO ' ||
+        history_table ||
+        '(' ||
+        array_to_string(commonColumns , ',') ||
+        ',' ||
+        quote_ident(sys_period) ||
+        ') VALUES ($1.' ||
+        array_to_string(commonColumns, ',$1.') ||
+        ',tstzrange($2, $3, ''[)''))')
+         USING OLD, range_lower, time_stamp_to_use;
+      END IF;
     END IF;
+
   END IF;
 
   IF TG_OP = 'UPDATE' OR TG_OP = 'INSERT' THEN
     manipulate := jsonb_set('{}'::jsonb, ('{' || sys_period || '}')::text[], to_jsonb(tstzrange(time_stamp_to_use, null, '[)')));
+
+    IF increment_version = 'true' THEN
+      manipulate := jsonb_set(manipulate, ('{' || version_column_name || '}')::text[], to_jsonb(existing_version + 1));
+    END IF;
 
     RETURN jsonb_populate_record(NEW, manipulate);
   END IF;
